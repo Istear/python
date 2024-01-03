305e7f4bef4945e595040c164368ed5f2757caea
Istear <istearahmed@gmail.com>
2024-01-03 16:17:33 +0600

    first commit

---
+++ syntex.py
@@ -0,0 +1,17 @@
+#white space and indentation
+def addSeries():
+    i=0
+    max=10
+    while(i<max):
+        print(i)
+        i=i+1
+
+addSeries()
+
+#Continuation of statements
+a=True
+b=False
+c=True
+if (a == True) and (b == False) and \
+   (c == True):
+    print("Continuation of statements")
